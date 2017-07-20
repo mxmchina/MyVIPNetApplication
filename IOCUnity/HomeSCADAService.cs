a8322e8aa8751af7448f442a3868676972ddd658
mxm <maxumin@126.com>
2017-07-20 10:19:31 +0800

    wcf 无添加引用提交

---
+++ IOCUnity/HomeSCADAService.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace IOCUnity
+{
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("System.ServiceModel", "4.0.0.0")]
+    [System.ServiceModel.ServiceContractAttribute(Namespace = "", ConfigurationName = "PPS2.Scada.HomeSCADAService")]
+    public interface HomeSCADAService
+    {
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetNowValueByTag", ReplyAction = "urn:HomeSCADAService/GetNowValueByTagResponse")]
+        System.Data.DataTable GetNowValueByTag(string tag);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetNowValueByTag", ReplyAction = "urn:HomeSCADAService/GetNowValueByTagResponse")]
+        System.Threading.Tasks.Task<System.Data.DataTable> GetNowValueByTagAsync(string tag);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetValueByTagDureTime", ReplyAction = "urn:HomeSCADAService/GetValueByTagDureTimeResponse")]
+        System.Data.DataTable GetValueByTagDureTime(string tag, System.DateTime dtStart, System.DateTime dtEnd);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetValueByTagDureTime", ReplyAction = "urn:HomeSCADAService/GetValueByTagDureTimeResponse")]
+        System.Threading.Tasks.Task<System.Data.DataTable> GetValueByTagDureTimeAsync(string tag, System.DateTime dtStart, System.DateTime dtEnd);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetLastValueByTag", ReplyAction = "urn:HomeSCADAService/GetLastValueByTagResponse")]
+        System.Data.DataTable GetLastValueByTag(string tag, System.DateTime lastDateTime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetLastValueByTag", ReplyAction = "urn:HomeSCADAService/GetLastValueByTagResponse")]
+        System.Threading.Tasks.Task<System.Data.DataTable> GetLastValueByTagAsync(string tag, System.DateTime lastDateTime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetLastValueByTag2", ReplyAction = "urn:HomeSCADAService/GetLastValueByTag2Response")]
+        System.Data.DataSet GetLastValueByTag2(string tag, System.DateTime lastDateTime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetLastValueByTag2", ReplyAction = "urn:HomeSCADAService/GetLastValueByTag2Response")]
+        System.Threading.Tasks.Task<System.Data.DataSet> GetLastValueByTag2Async(string tag, System.DateTime lastDateTime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetHistoryValueByTag", ReplyAction = "urn:HomeSCADAService/GetHistoryValueByTagResponse")]
+        System.Data.DataTable GetHistoryValueByTag(string tag, System.DateTime currentDateTime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetHistoryValueByTag", ReplyAction = "urn:HomeSCADAService/GetHistoryValueByTagResponse")]
+        System.Threading.Tasks.Task<System.Data.DataTable> GetHistoryValueByTagAsync(string tag, System.DateTime currentDateTime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetScadaValueOPCDataByTag", ReplyAction = "urn:HomeSCADAService/GetScadaValueOPCDataByTagResponse")]
+        System.Data.DataTable GetScadaValueOPCDataByTag(string strTag);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetScadaValueOPCDataByTag", ReplyAction = "urn:HomeSCADAService/GetScadaValueOPCDataByTagResponse")]
+        System.Threading.Tasks.Task<System.Data.DataTable> GetScadaValueOPCDataByTagAsync(string strTag);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetScadaValueOPCDataByTags", ReplyAction = "urn:HomeSCADAService/GetScadaValueOPCDataByTagsResponse")]
+        System.Data.DataTable GetScadaValueOPCDataByTags(string[] strTags);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/GetScadaValueOPCDataByTags", ReplyAction = "urn:HomeSCADAService/GetScadaValueOPCDataByTagsResponse")]
+        System.Threading.Tasks.Task<System.Data.DataTable> GetScadaValueOPCDataByTagsAsync(string[] strTags);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/ExecuteOilAmountService", ReplyAction = "urn:HomeSCADAService/ExecuteOilAmountServiceResponse")]
+        void ExecuteOilAmountService(System.DateTime dtime);
+
+        [System.ServiceModel.OperationContractAttribute(Action = "urn:HomeSCADAService/ExecuteOilAmountService", ReplyAction = "urn:HomeSCADAService/ExecuteOilAmountServiceResponse")]
+        System.Threading.Tasks.Task ExecuteOilAmountServiceAsync(System.DateTime dtime);
+    }
+}
--- IOCUnity/Program.cs
+++ IOCUnity/Program.cs
@@ -8,6 +8,10 @@ using System.Configuration;
 using System.IO;
 using Microsoft.Practices.Unity.Configuration;
 using Mxm.Interface;
+using System.Net;
+using System.ServiceModel;
+using System.Security.Cryptography.X509Certificates;
+using System.Net.Security;
 
 namespace IOCUnity
 {
@@ -15,21 +19,68 @@ namespace IOCUnity
     {
         static void Main(string[] args)
         {
-
+            try
             {
-                IUnityContainer container = new UnityContainer();
-                ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
-                fileMap.ExeConfigFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "CfgFiles\\Unity.Config.xml");//找配置文件的路径
-                Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
-                UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection(UnityConfigurationSection.SectionName);
+                {
+                    string serviceUrl = "http://11.10.166.74:8090/SCADA/HomeSCADAService.svc";
 
-                section.Configure(container, "testContainer");//注册
 
-                Iphone phone = container.Resolve<Iphone>();
-                phone.Call();
+                    ServicePointManager.ServerCertificateValidationCallback = delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
 
-                Console.Read();
+                    BasicHttpBinding binding;
+                    Uri address = new Uri(serviceUrl);
+                    if (address.Scheme == "https")
+                    {
+                        binding = new BasicHttpBinding(BasicHttpSecurityMode.Transport);
+                        binding.Security.Transport.ClientCredentialType = HttpClientCredentialType.Ntlm;
+                    }
+                    else
+                    {
+                        binding = new BasicHttpBinding(BasicHttpSecurityMode.None);
+
+                    }
+
+                    binding.CloseTimeout = new TimeSpan(3, 0, 0);
+                    binding.OpenTimeout = new TimeSpan(3, 0, 0);
+                    binding.ReceiveTimeout = new TimeSpan(3, 0, 0);
+                    binding.SendTimeout = new TimeSpan(3, 0, 0);
+                    binding.MaxReceivedMessageSize = 65536000;
+                    //binding.MessageEncoding = WSMessageEncoding.Text;
+                    //binding.TextEncoding = Encoding.UTF8;
+
+                    EndpointAddress addr = new EndpointAddress(serviceUrl);
+
+                    ChannelFactory<HomeSCADAService> _serviceFactory;
+                    _serviceFactory = new ChannelFactory<HomeSCADAService>(binding, addr);
+
+                    HomeSCADAService service = _serviceFactory.CreateChannel();
+
+                    service.ExecuteOilAmountService(DateTime.Parse("2017-7-12 07:31"));
+
+                }
+
+
+                {
+                    IUnityContainer container = new UnityContainer();
+                    ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap();
+                    fileMap.ExeConfigFilename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory + "CfgFiles\\Unity.Config.xml");//找配置文件的路径
+                    Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(fileMap, ConfigurationUserLevel.None);
+                    UnityConfigurationSection section = (UnityConfigurationSection)configuration.GetSection(UnityConfigurationSection.SectionName);
+
+                    section.Configure(container, "testContainer");//注册
+
+                    Iphone phone = container.Resolve<Iphone>();
+                    phone.Call();
+
+                    Console.Read();
+                }
             }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            
         }
     }
 }
